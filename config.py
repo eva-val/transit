a35443a066ca1ab559d25e3e95991ba1fdd38359
Eva Valentine <stephanie.howanietz@accucodeai.com>
2025-03-18 09:48:49 -0700

    Add .env

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class config(BaseSettings):
@@ -7,6 +7,7 @@ class config(BaseSettings):
     ROUTE_IDS: list[str] = ["40_100479", "40_2LINE", "1_100146"]
     UPDATE_INTERVAL: int = 60  # seconds
     DEFAULT_ZOOM: int = 12
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 Config = config()
